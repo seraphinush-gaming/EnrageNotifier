27b8c2f07171416cc9907a123606d7e87e5062a6
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-01-16 15:42:37 +0900

    1.10 r:01

--- index.js
+++ index.js
@@ -1,83 +1,126 @@
-const Command = require('command')
+// OPCODE REQUIRED :
+// - S_BOSS_GAGE_INFO
+// - S_DESPAWN_NPC
+// - S_DUNGEON_EVENT_MESSAGE
+// - S_LOAD_TOPO
+// - S_NPC_STATUS
 
-module.exports = function EnrageNotifier(dispatch) {
-    let hpMax,
-		hpCurrent,
-		hpPercent,
-		nextEnrage,
-		enabled = true,
-		inHH = false,
-		wasEnraged = 0,
-		bosses = new Set(),
-		alert = true // if true, shows an additional alert in the middle of your screen
-	
-	dispatch.hook('S_LOAD_TOPO', 1, event => {
-		inHH = event.zone == 9950
-	})
+// Version 1.10 r:01
 
-    dispatch.hook('S_BOSS_GAGE_INFO', 3, (event) => {
-        bosses.add("" + event.id)
-        hpMax = event.maxHp
-        hpCurrent = event.curHp
-        hpPercent = Math.floor((hpCurrent / hpMax) * 100)
-        nextEnrage = (hpPercent > 10) ? (hpPercent - 10) : 0
-    })
+module.exports = function MessageEnrage(d) {
 
-    dispatch.hook('S_NPC_STATUS', 1, (event) => {
-		if(!enabled || inHH) return
-        if(!(bosses.has("" + event.creature))) return
+    let enable = true,
+        notice = true,
+        inHH = false
 
-        if((event.enraged == 0) && (wasEnraged == 1)) {
-            wasEnraged = 0
-            let messageString = '<font color="#FFFFFF" size="25">Next Enrage at </font><font color="#FF0000" size="30">' + nextEnrage + '%</font>'
-            if(nextEnrage > 0) {
-				if(alert) notify(messageString)
-				notifyChat(messageString)
-			}
-        }
+    let boss = new Set(),
+        enraged = false,
+        hpMax = -1,
+        hpCur = -1,
+        hpPer = -1,
+        nextEnrage = -1,
+        timeout = -1,
+        timeoutCounter = -1
 
-        if((event.enraged == 1) && (wasEnraged == 0)) {
-            wasEnraged = 1
-            if(alert) notify('<font color="#FF0000" size="50">Boss Enraged!</font>')
+    // code
+    d.hook('S_LOAD_TOPO', (e) => {
+        (e.zone === 9950) ? inHH = true : inHH = false
+        if (timeout !== -1) {
+            clearTimeout(timeout)
+            clearTimeout(timeoutCounter)
+            timeout = -1
+            timeoutCounter = -1
         }
     })
 
-    dispatch.hook('S_DESPAWN_NPC', 1, (event) => {
-		if(bosses.delete("" + event.target)) wasEnraged = 0
+    d.hook('S_BOSS_GAGE_INFO', (e) => {
+        if (!enable || inHH) return
+        boss.add(e.id.toString())
+        hpMax = e.maxHp
+        hpCur = e.curHp
+        hpPer = Math.floor((hpCur / hpMax) * 100)
+        nextEnrage = (hpPer > 10) ? (hpPer - 10) : 0
     })
-	
-	function notify(msg) {
-		dispatch.toClient('S_DUNGEON_EVENT_MESSAGE', 1, {
-            unk1: 42, // 42 Blue Shiny Text, 31 Normal Text
+
+    d.hook('S_NPC_STATUS', (e) => {
+        if (!enable || inHH) return
+        if (!boss.has(e.creature.toString())) return
+        if (e.enraged === 1 && !enraged) {
+            enraged = true
+            toChat(`Boss enraged`)
+            timeout = setTimeout(timeRemaining, 26000)
+        } else if (e.enraged === 0 && enraged) {
+            if (hpPer === 100) return
+            enraged = false
+            send(`Next enrage at ` + `${nextEnrage}`.clr('FF0000') + `%`.clr('FFFFFF'))
+            clearTimeout(timeout)
+            clearTimeout(timeoutCounter)
+            timeout = -1
+            timeoutCounter = -1
+        }
+    })
+
+    d.hook('S_DESPAWN_NPC', (e) => {
+        if (!enable || inHH) return
+        if (boss.has(e.gameId.toString())) {
+            boss.delete(e.gameId.toString())
+            clearTimeout(timeout)
+            clearTimeout(timeoutCounter)
+            timeout = -1
+            timeoutCounter = -1
+            enraged = false
+        }
+    })
+
+    // helper
+    function toChat(msg) {
+        if (notice) d.toClient('S_DUNGEON_EVENT_MESSAGE', {
+            unk1: 31, // 42 blue shiny text, 31 normal Text
             unk2: 0,
             unk3: 27,
             message: msg
         })
-	}
-	
-	function notifyChat(msg) {
-		command.message(' ' + msg)
-	}
-	
-	// ################# //
-	// ### Chat Hook ### //
-	// ################# //
-	
-	const command = Command(dispatch)
-	command.add('enrage', (param) => {
-		if(param == null) {
-			enabled = !enabled
-			command.message('[Enrage Notifier] ' + (enabled ? '<font color="#56B4E9">enabled</font>' : '<font color="#E69F00">disabled</font>'))
-			console.log('[Enrage Notifier] ' + (enabled ? 'enabled' : 'disabled'))
-		}
-		else if(param == "alert") {
-			alert = !alert
-			command.message('[Enrage Notifier] alerts ' + (alert ? '<font color="#56B4E9">enabled</font>' : '<font color="#E69F00">disabled</font>'))
-			console.log('[Enrage Notifier] alerts ' + (alert ? 'enabled' : 'disabled'))
-		}
-		else command.message('Commands:<br>'
-							+ ' "enrage" (enable/disable EnrageNotifier),<br>'
-							+ ' "enrage alert" (enable/disable alerts in the center of your screen)'
-			)
-	})
+        else send(msg)
+    }
+
+    function timeRemaining() {
+        let i = 10
+        timeoutCounter = setInterval( () => {
+            if (enraged && i > 0) {
+                toChat(`Time remaining : ` + `${i}`.clr('FF0000') + ` seconds`.clr('FFFFFF'))
+                i--
+            } else {
+                clearInterval(timeoutCounter)
+                timeoutCounter = -1
+            }
+        }, 1000)
+    }
+
+    // command
+    try {
+        const Command = require('command')
+        const command = Command(d)
+        command.add('enrage', (arg) => {
+            // toggle
+            if (!arg) {
+                enable = !enable
+                status()
+            // notice
+            } else if (arg === 'notice') {
+                notice = !notice
+                send(`Notice to screen ${notice ? 'enabled'.clr('56B4E9') : 'disabled'.clr('E69F00')}` + `.`.clr('FFFFFF'))
+            // status
+            } else if (arg === 'status') status()
+            else send(`Invalid argument.`.clr('FF0000'))
+        })
+        function send(msg) { command.message(`[message-enrage] : ` + [...arguments].join('\n\t - ')) }
+        function status() { send(
+            `Enrage message ${enable ? 'enabled'.clr('56B4E9') : 'disabled'.clr('E69F00')}` + `.`.clr('FFFFFF'),
+            `Notice to screen : ${notice ? 'enabled' : 'disabled'}`) 
+        }
+    } catch (e) { console.log(`[ERROR] -- message-enrage module --`) }
+    
 }
+
+// credit : https://github.com/Some-AV-Popo
+String.prototype.clr = function (hexColor) { return `<font color="#${hexColor}">${this}</font>` }
